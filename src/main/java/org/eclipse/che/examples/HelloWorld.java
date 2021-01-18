1cc264f7f5b98dac1bbffd4dfc2e5bc8a3ad3870
fgonzalezmm <fgonzalez@mutua.es>
2021-01-18 11:07:40 +0000

    first push

---
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package org.eclipse.che.examples;
+
+public class HelloWorld {
+    public static void main(String... argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
+/*        for (Person p : roster) {
+            if (p.getGender() == Person.Sex.MALE) {
+                System.out.println(p.getName());
+            }
+        }
+*/
+    }
+}
---
+++ src/main/java/org/eclipse/che/examples/Person.java
@@ -0,0 +1,7 @@
+package org.eclipse.che.examples;
+import lombok.Data
+
+@Data
+public class Person {
+    private String name;
+}
